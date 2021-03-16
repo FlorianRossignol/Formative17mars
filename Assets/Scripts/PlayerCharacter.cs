7bd80311d1ce322740c23808d0ecc79cf2893a87
FlorianRossignol <rossignol.florian.fr@gmail.com>
2021-03-16 14:57:55 +0100

    adding script on player character

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer PlayerSprite_;
+    [SerializeField] private Rigidbody2D PlayerBody_;
+    private const float MoveSpeed_ = 2.0f;
+    private bool FacingRight_ = false;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void FixedUpdate()
+    {
+        PlayerBody_.velocity = new Vector2(Input.GetAxis("Horizontal") * MoveSpeed_, PlayerBody_.velocity.y);
+        PlayerBody_.velocity = new Vector2(PlayerBody_.velocity.x, Input.GetAxis("Vertical") * MoveSpeed_);
+    }
+}
