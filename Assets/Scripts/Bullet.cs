51c6d1fd6a7063178b5c2622b84bd07bb79cca39
FlorianRossignol <rossignol.florian.fr@gmail.com>
2021-03-16 16:20:48 +0100

    add some prefabs and scripts

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private GameObject HitEffect_;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect_ = Instantiate(HitEffect_, transform.position, Quaternion.identity);
+        Destroy(effect_, 5f);
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField] private Transform FirePoint_;
+    [SerializeField] private GameObject BulletPrefab_;
+    private float BulletForce_ = 20.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+       GameObject PlayerBullet = Instantiate(BulletPrefab_, FirePoint_.position, FirePoint_.rotation);
+       Rigidbody2D PlayerBody_ = PlayerBullet.GetComponent<Rigidbody2D>();
+        PlayerBody_.AddForce(FirePoint_.up * BulletForce_, ForceMode2D.Impulse);
+    }    
+}
