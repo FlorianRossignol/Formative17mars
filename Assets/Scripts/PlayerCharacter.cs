b0303b12e56759ac681570f1f6444ce05dda9be6
FlorianRossignol <rossignol.florian.fr@gmail.com>
2021-03-16 15:44:59 +0100

    work on charactersprite

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -6,19 +6,32 @@ public class PlayerCharacter : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer PlayerSprite_;
     [SerializeField] private Rigidbody2D PlayerBody_;
-    private const float MoveSpeed_ = 2.0f;
+    private const float MoveSpeed_ = 5.0f;
     private bool FacingRight_ = false;
-
-
+    private Vector2 movement_;
+    [SerializeField] private Camera cam_;
+    private Vector2 MousePos_;
     void Start()
     {
-        
+        PlayerBody_ = GetComponent<Rigidbody2D>();
     }
 
-    
+    private void Update()
+    {
+        movement_.x = Input.GetAxisRaw("Horizontal");
+        movement_.y = Input.GetAxisRaw("Vertical");
+
+       MousePos_ = cam_.ScreenToWorldPoint(Input.mousePosition);
+    }
+
     void FixedUpdate()
     {
-        PlayerBody_.velocity = new Vector2(Input.GetAxis("Horizontal") * MoveSpeed_, PlayerBody_.velocity.y);
-        PlayerBody_.velocity = new Vector2(PlayerBody_.velocity.x, Input.GetAxis("Vertical") * MoveSpeed_);
+        PlayerBody_.MovePosition(PlayerBody_.position + movement_ * MoveSpeed_ * Time.fixedDeltaTime);
+
+        Vector2 LookDir = MousePos_ - PlayerBody_.position;
+        float Angle_ = Mathf.Atan2(LookDir.y, LookDir.x) * Mathf.Rad2Deg - 90f;
+
+        PlayerBody_.rotation = Angle_;
     }
+
 }
