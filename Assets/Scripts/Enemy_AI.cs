98b0163e60a6406f15f73697de0aea12ba6720cd
SWoolman <sandy@woolmans.net>
2021-04-07 15:15:01 +0200

    added assets

---
+++ Assets/Scripts/Enemy_AI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Enemy_AI : MonoBehaviour
+{
+    public float accelerationTime = 2f;
+    public float maxSpeed = 5f;
+    private Vector2 movement;
+    private float timeLeft;
+    private Rigidbody2D rb;
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0)
+        {
+            movement = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+            timeLeft += accelerationTime;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(movement * maxSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("bullet"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
