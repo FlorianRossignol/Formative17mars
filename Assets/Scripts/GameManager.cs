528b4a82a03148d07f6a4c002ed341db2150fcff
FlorianRossignol <rossignol.florian.fr@gmail.com>
2021-04-26 17:09:30 +0200

    adding gamemanagerscript

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+   [SerializeField]  List<GameObject> Enemis_;
+    [SerializeField] private GameObject EnemisGameObjects_;
+
+    public int Enemiscount_ = Enemiscount_;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if(Enemis_ == 0)
+        {
+            SceneManager.LoadScene("Stage2");
+        }
+    }
+}
