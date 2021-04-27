c42a5a755ba50835b94e8c8a683851f563b20283
SWoolman <sandy@woolmans.net>
2021-04-27 14:21:08 +0200

    added script for game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,28 +4,32 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
-   [SerializeField] List<GameObject> Enemis_;
-   [SerializeField] private float EnemisCount_ = 10;
+   //[SerializeField] List<GameObject> Enemis_;
+   [SerializeField] private GameObject[] enemy;
 
-    void Start()
-    {
-        EnemisCount_ = 10;
-    }
 
+   /*void Start()
+   {
+       EnemisCount_ = 10;
+   }
+*/
     
     void Update()
     {
-        if(EnemisCount_ == 0)
+        enemy = GameObject.FindGameObjectsWithTag("enemy");
+
+        if (enemy.Length == 0)
         {
             SceneManager.LoadScene("Stage2");
         }
     }
+    
 
-    private void EnemisKilled()
+   /* private void EnemisKilled()
     {
         if(Enemis_ != null)
         {
             EnemisCount_--;
         }
-    }
+    }*/
 }
